1eef3cbdb30eb5d667861a3a9fc21b9d3f437d90
AsadaYouko <126053808+XianyunSun@users.noreply.github.com>
2023-05-15 11:37:10 +0800

    Update train_id.sh

--- train_id.sh
+++ train_id.sh
@@ -3,5 +3,5 @@ CUDA_VISIBLE_DEVICES=3 python train_id.py \
     --train_csv /sdata/xianyun.sun/SynthASpoof_data_crop/labels.csv \
     --test_dir /sdata/xianyun.sun/casia_MFSD_img/test_release_crop  \
     --test_csv /sdata/xianyun.sun/casia_MFSD_img/labels.csv  \
-    --train_epoch 1  \
+    --train_epoch 30  \
     --log train_id
